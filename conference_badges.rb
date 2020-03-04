13cd8b8cc801c1341a3b1a8ae11c977583196c05
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-04 23:51:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(array)
+  room_number = 1
+  
+  array.each do |speaker|
+    return "Hello, #{speaker}!You'll be assigned to room #{room_number}"
+    room_number += 1
+  end
+  
+end  
+  
