572f1f68e5bb3f5e0d8c6ddd40650ce515037088
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 00:02:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,9 @@ def batch_badge_creator(array)
   badge_messages = []
   
   array.each do |name|
-    return badge_messages.push(badge_maker(name)) 
+    badge_messages.push(badge_maker(name)) 
   end
-  
+  return badge_messages
 end
 #--------------------
 def assign_rooms(array)
